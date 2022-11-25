7c194d2a79f2aa9a579fc3a050742e8fef3c05c9
isus-ipanienko <preliga@proton.me>
2022-11-25 01:09:19 +0100

    add packages

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ DOTFILES=$HOME/.dotfiles
 if [ "$1" == "--fresh" ]
 then
     echo "installing dependencies..."
-    sudo pacman --noconfirm --needed -Sy stow base-devel git \
+    sudo pacman --noconfirm --needed -Sy stow base-devel git lazygit \
             curl ca-certificates ntp dosfstools exfat-utils ntfs-3g \
             xorg-server xorg-xinit xorg-xmodmap acpilight \
             networkmanager nm-connection-editor gnome-keyring \
-            pipewire-pulse wireplumber pamixer \ 
+            pipewire-pulse wireplumber pamixer noto-fonts noto-fonts-emoji \ 
             lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings atool \
             i3-wm picom polybar dmenu slock feh dunst scrot python-pywal \
-            noto-fonts noto-fonts-emoji \
-            kitty zsh ranger mpv tmux neovim fzf ripgrep firefox
+            kitty zsh tmux neovim fzf ripgrep \
+            ranger mpv firefox flatpak xdg-desktop-portal-gnome
 
     sudo systemctl enable lightdm
     sudo systemctl enable NetworkManager
@@ -21,6 +21,10 @@ then
     git clone https://github.com/isus-ipanienko/zsh-fzf $HOME/.zsh/zsh-fzf
     git clone https://github.com/isus-ipanienko/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
     git clone https://github.com/isus-ipanienko/zsh-syntax-highlighting $HOME/.zsh/zsh-syntax-highlighting
+
+    flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    flatpak install flathub com.discordapp.Discord
+    flatpak install flathub org.keepassxc.KeePassXC
 
     echo "password: "
     chsh -s /bin/zsh $USER >/dev/null 2>&1
