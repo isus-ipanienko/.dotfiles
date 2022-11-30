6c26fd4353b7f124920cf9777c5ade81312366a7
isus-ipanienko <preliga@proton.me>
2022-11-30 23:03:32 +0100

    fixed lua lsp config for neovim

--- nvim/.config/nvim/lua/ipanienko/keybinds.lua
+++ nvim/.config/nvim/lua/ipanienko/keybinds.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>E', ':.,.+')
 vim.keymap.set('x', '<leader>p', '"_dP')
 
 vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<cr>', { silent = true })
--- nvim/.config/nvim/lua/ipanienko/lsp.lua
+++ nvim/.config/nvim/lua/ipanienko/lsp.lua
@@ -22,13 +22,38 @@ local on_attach = function(client, bufnr)
     end, bufopts)
 end
 
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'sumneko_lua' }
+local lspconfig = require 'lspconfig'
+local servers = { 'clangd', 'rust_analyzer', 'pyright' }
 for _, lsp in ipairs(servers) do
-    require 'lspconfig'[lsp].setup {
+    lspconfig[lsp].setup {
         on_attach = on_attach,
         capabilities = require 'cmp_nvim_lsp'.default_capabilities(),
     }
 end
+
+lspconfig['sumneko_lua'].setup {
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+}
 
 local luasnip = require 'luasnip'
 luasnip.config.set_config {
