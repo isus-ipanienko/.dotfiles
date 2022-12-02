c15ea7ee15c3d10ecf62a3f8831894c944564c84
isus-ipanienko <preliga@proton.me>
2022-12-02 21:36:56 +0100

    added fzf-native

--- nvim/.config/nvim/lua/ipanienko/plugins.lua
+++ nvim/.config/nvim/lua/ipanienko/plugins.lua
@@ -26,6 +26,7 @@ return require 'packer'.startup(function(use)
 
     use 'nvim-lua/plenary.nvim'
     use 'nvim-telescope/telescope.nvim'
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
     use 'ThePrimeagen/harpoon'
 
     use 'kdheepak/lazygit.nvim'
