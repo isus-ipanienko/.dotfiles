cd45eac0a440b6da871df1aaec4cfa7c214f9861
isus-ipanienko <preliga@proton.me>
2023-02-24 21:25:38 +0100

    cleanup, removed gaps, changed some software

--- install.sh
+++ install.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
-STOW_FOLDERS="i3,kitty,nvim,picom,polybar,ranger,tmux,wallpapers,zsh,X11,rofi,scripts"
+STOW_FOLDERS="X11,i3,kitty,nvim,picom,polybar,rofi,scripts,tmux,wallpapers,zsh"
 DOTFILES=$HOME/.dotfiles
 
 if [ "$1" == "--fresh" ]
 then
     echo "installing dependencies..."
-    sudo pacman --noconfirm --needed -Sy stow base-devel git lazygit p7zip \
-            curl ca-certificates ntp dosfstools exfat-utils ntfs-3g \
-            xorg-server xorg-xinit xorg-xmodmap acpilight ttf-sazanami \
+    sudo pacman --noconfirm --needed -Sy stow \
+            xorg-server xorg-xinit xorg-xmodmap xdotool acpilight ttf-sazanami \
+            i3-wm picom polybar rofi rofimoji slock feh dunst scrot python-pywal \
+            curl ca-certificates ntp dosfstools exfat-utils ntfs-3g p7zip \
             networkmanager network-manager-applet gnome-keyring openvpn \
-            pipewire-pulse wireplumber pamixer networkmanager-openvpn \ 
+            pipewire-pulse wireplumber pamixer networkmanager-openvpn \
             lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings atool \
-            i3-wm picom polybar rofi xdotool rofimoji slock feh dunst scrot python-pywal \
-            kitty zsh tmux neovim fzf ripgrep \
+            base-devel git gitui kitty zsh tmux neovim fzf ripgrep \
             clang llvm lldb rustup rust-analyzer pyright lua-language-server \
-            ranger mpv firefox flatpak xdg-desktop-portal-gnome
-            ranger mpv firefox keepassxc flatpak xdg-desktop-portal-gnome
+            ncspot mpv firefox keepassxc flatpak xdg-desktop-portal-gnome
 
     sudo systemctl enable lightdm
     sudo systemctl enable NetworkManager
@@ -25,17 +24,14 @@ then
     git clone https://github.com/isus-ipanienko/zsh-syntax-highlighting $HOME/.zsh/zsh-syntax-highlighting
 
     mkdir $HOME/Git
-    git clone https://aur.archlinux.org/nerd-fonts-noto.git $HOME/Git
     git clone https://aur.archlinux.org/nerd-fonts-noto.git $HOME/Git/nerd-fonts-noto
+    git clone https://aur.archlinux.org/wiki-tui.git $HOME/Git/wiki-tui
 
     flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
     flatpak install flathub com.discordapp.Discord
-    flatpak install flathub com.spotify.Client
 
     echo "password: "
     chsh -s /bin/zsh $USER >/dev/null 2>&1
-
-    7z x -o $DOTFILES/wallpapers/.config/wallpapers $DOTFILES/wallpapers/.config/wallpapers.7z    
 
     chmod +x polybar/.config/polybar/*.sh
 
