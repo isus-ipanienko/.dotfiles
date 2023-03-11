826ef4685ef7bd8754de190e7f8c37a85a15b622
isus-ipanienko <preliga@proton.me>
2023-03-11 02:00:28 +0100

    snippets and keybinds

--- nvim/.config/nvim/lua/ipanienko/snippets.lua
+++ nvim/.config/nvim/lua/ipanienko/snippets.lua
@@ -30,59 +30,90 @@ ls.add_snippets("all", {
     })
 })
 
-ls.add_snippets("rust", {
-
-  s('derivedebug', t '#[derive(Debug)]'),
-  s('deadcode', t '#[allow(dead_code)]'),
-  s('allowfreedom', t '#![allow(clippy::disallowed_names, unused_variables, dead_code)]'),
-
-  s('clippypedantic', t '#![warn(clippy::all, clippy::pedantic)]'),
-
-  s(':turbofish', { t {'::<'}, i(0), t {'>'} }),
-
-  s('print', {
-    -- t {'println!("'}, i(1), t {' {:?}", '}, i(0), t {');'}}),
-    t {'println!("'}, i(1), t {' {'}, i(0), t {':?}");'}}),
-
-  s('for',
-  {
-  t {'for '}, i(1), t {' in ' }, i(2), t {' {', ''},
-        i(0),
-  t {'}', ''},
-  }),
-
-  s('struct',
-  {
-    t {'#[derive(Debug)]', ''},
-    t {'struct '}, i(1), t {' {', ''},
-      i(0),
-    t {'}', ''},
-  }),
-
-  s('test',
-  {
-      t {'#[test]', ''},
-      t {'fn '}, i(1), t {'() {', ''},
-      t {'	assert'}, i(0), t {'', ''},
-      t {'}'},
-  }),
-
-  s('testcfg',
-  {
-    t {'#[cfg(test)]', ''},
-    t {'mod '}, i(1), t {' {', ''},
-    t {'	#[test]', ''},
-    t {'	fn '}, i(2), t {'() {', ''},
-    t {'		assert'}, i(0), t {'', ''},
-    t {'	}', ''},
-    t {'}'},
-  }),
-
-  s('if',
-  {
-    t {'if '}, i(1), t {' {', ''},
-     i(0),
-    t {'}'},
-  }),
+ls.add_snippets("cpp", {
+    s("class",
+        fmt(
+            [[
+            class {}
+            {{
+            public:
+                {}() = default;
+                {}({}&&) = default;
+                {}(const {}&) = default;
+                {} &operator=({}&&) = default;
+                {} &operator=(const {}&) = default;
+                ~{}() = default;
+            private:
+                {}
+            }};
+            ]],
+            {
+                i(1),
+                rep(1),
+                rep(1), rep(1),
+                rep(1), rep(1),
+                rep(1), rep(1),
+                rep(1), rep(1),
+                rep(1),
+                i(0),
+            }
+        )
+    ),
 })
 
+ls.add_snippets("rust", {
+
+    s("derive",
+        fmt(
+            [[
+            #[derive({})]
+            ]],
+            {
+                i(0, "Debug")
+            }
+        )
+    ),
+
+    s("print", { t { "println!(\"" }, i(1), t { " {" }, i(0), t { ":?}\");" } }),
+
+    s("for",
+        {
+            t { "for " }, i(1), t { " in " }, i(2), t { " {", "" },
+            i(0),
+            t { "}", "" },
+        }),
+
+    s("struct",
+        {
+            t { "#[derive(Debug)]", "" },
+            t { "struct " }, i(1), t { " {", "" },
+            i(0),
+            t { "}", "" },
+        }),
+
+    s("test",
+        {
+            t { "#[test]", "" },
+            t { "fn " }, i(1), t { "() {", "" },
+            t { "	assert" }, i(0), t { "", "" },
+            t { "}" },
+        }),
+
+    s("testcfg",
+        {
+            t { "#[cfg(test)]", "" },
+            t { "mod " }, i(1), t { " {", "" },
+            t { "	#[test]", "" },
+            t { "	fn " }, i(2), t { "() {", "" },
+            t { "		assert" }, i(0), t { "", "" },
+            t { "	}", "" },
+            t { "}" },
+        }),
+
+    s("if",
+        {
+            t { "if " }, i(1), t { " {", "" },
+            i(0),
+            t { "}" },
+        }),
+})
