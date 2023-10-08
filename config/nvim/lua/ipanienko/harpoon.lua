07fd1ef8dfb12cc6d9ce4079cdb9bcae94dc7c13
isus-ipanienko <preliga@proton.me>
2023-10-08 12:57:03 +0200

    Streamlined nvim config

--- config/nvim/lua/ipanienko/harpoon.lua
+++
@@ -1,9 +0,0 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-
-vim.keymap.set("n", "<leader>pa", mark.add_file, { silent = true })
-vim.keymap.set("n", "<leader>po", ui.toggle_quick_menu, { silent = true })
-vim.keymap.set("n", "<C-1>", function() ui.nav_file(1) end, { silent = true })
-vim.keymap.set("n", "<C-2>", function() ui.nav_file(2) end, { silent = true })
-vim.keymap.set("n", "<C-3>", function() ui.nav_file(3) end, { silent = true })
-vim.keymap.set("n", "<C-4>", function() ui.nav_file(4) end, { silent = true })
